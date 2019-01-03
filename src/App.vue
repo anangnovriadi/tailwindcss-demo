14120521eda4b626d07d42febe5c8ab4bd480647
nero adaware <nero2009@rocketmail.com>
2019-01-03 16:01:31 +0100

    initial commit :tada:

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="flex justify-center items-center">
+    <div class="login-container shadow-lg">
+      <div class="float-right">
+        <font-awesome-icon size="3x" icon="times" class=" m-4"/>
+        <div class="">
+          <input type="checkbox" class=""/>
+        </div>
+      </div>
+      <div class="login-curve">
+        <div class="login-form">
+            <h1 class="login-form__header text-blue-darker text-4xl">Sign up</h1>
+            <div class="login-form__input px-24">
+              <form>
+                <div class="mb-8">
+                  <label class="mb-2 text-xl text-blue-darker">First and last name</label>
+                  <input type="text" class="h-12 w-3/4"/>
+                </div>
+                <div class="mb-8">
+                  <label class="mb-2 text-xl text-blue-darker">Email</label>
+                  <input type="text" class="h-12 w-3/4"/>
+                </div>
+                <div class="mb-8">
+                  <label class="mb-2 text-xl text-blue-darker">Username</label>
+                  <input type="text" class="h-12 w-3/4"/>
+                </div>
+                <div class="mb-8">
+                  <label class="mb-2 text-xl text-blue-darker">Password</label>
+                  <input type="text" class="h-12 w-3/4"/>
+                </div>
+              </form>
+              <p class="text-blue-dark">Minimum 8 letter, at least one letter and one special symbol</p>
+            </div>
+        </div>
+      </div>
+      
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -21,8 +50,26 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #ff9999;
+  height: 100vh;
+  width: 100%;
+}
+input, label {
+    display:block;
+}
+.login-container{
+  height: 750px;
+  width: 60%;
+  background-color: #ff8080 ;
+}
+.login-curve{
+  height: 100%;
+  width: 65%;
+  background-color: #f2f2f2;
+
+}
+.login-form__header{
+  padding: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/tailwind.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTimes)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 

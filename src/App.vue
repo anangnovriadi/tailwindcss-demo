f00055dfba7af644e2e8414bbba8b07c31ac2c13
nero adaware <nero2009@rocketmail.com>
2019-01-04 15:09:06 +0100

    Styling added

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,39 @@
 <template>
-  <div id="app" class="flex justify-center items-center">
-    <div class="login-container shadow-lg">
+  <div id="app" class="flex justify-center items-center w-full min-h-screen">
+    <div class="login-container shadow-lg rounded-lg w-3/5">
       <div class="float-right">
         <font-awesome-icon size="3x" icon="times" class=" m-4"/>
-        <div class="">
-          <input type="checkbox" class=""/>
-        </div>
       </div>
-      <div class="login-curve">
+      <div class="login-curve rounded-lg bg-white h-full w-3/5">
         <div class="login-form">
-            <h1 class="login-form__header text-blue-darker text-4xl">Sign up</h1>
-            <div class="login-form__input px-24">
+            <h1 class="p-16 text-blue-darker text-4xl">Sign up</h1>
+            <div class="login-form__input px-32 ">
               <form>
                 <div class="mb-8">
-                  <label class="mb-2 text-xl text-blue-darker">First and last name</label>
-                  <input type="text" class="h-12 w-3/4"/>
+                  <label class="mb-2 text-xl text-blue-darker">Name</label>
+                  <input type="text" class="h-12 w-full bg-grey-lighter"/>
                 </div>
                 <div class="mb-8">
                   <label class="mb-2 text-xl text-blue-darker">Email</label>
-                  <input type="text" class="h-12 w-3/4"/>
+                  <input type="text" class="h-12 w-full bg-grey-lighter"/>
                 </div>
                 <div class="mb-8">
                   <label class="mb-2 text-xl text-blue-darker">Username</label>
-                  <input type="text" class="h-12 w-3/4"/>
+                  <input type="text" class="h-12 w-full bg-grey-lighter"/>
                 </div>
                 <div class="mb-8">
                   <label class="mb-2 text-xl text-blue-darker">Password</label>
-                  <input type="text" class="h-12 w-3/4"/>
+                  <input type="text" class="h-12 w-full bg-grey-lighter"/>
+                </div>
+                <div>
+                  <input type="checkbox"/>
+                  <p class="ml-2 text-grey">I accept the terms and conditions</p>
+                </div>
+                <div>
+                  <button class="w-full mt-4 bg-blue-dark h-10 text-white hover:bg-blue-light hover:text-grey-light hover:border-blue-dark transition transition-slow transition-property-all transition-timing-linear">Signup</button>
                 </div>
               </form>
-              <p class="text-blue-dark">Minimum 8 letter, at least one letter and one special symbol</p>
+              
             </div>
         </div>
       </div>
@@ -46,30 +50,23 @@ export default {
 </script>
 
 <style>
+html { height: 100% }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   background-color: #ff9999;
-  height: 100vh;
-  width: 100%;
 }
-input, label {
+input[type="text"], label {
     display:block;
+}
+input[type="checkbox"], p {
+    display:inline-block;
 }
 .login-container{
   height: 750px;
-  width: 60%;
   background-color: #ff8080 ;
 }
-.login-curve{
-  height: 100%;
-  width: 65%;
-  background-color: #f2f2f2;
 
-}
-.login-form__header{
-  padding: 60px;
-}
 </style>
